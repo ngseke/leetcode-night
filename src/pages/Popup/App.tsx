0e520f7ae4bc69a22c918c8f8659c70ef1529db8
Huang Xingqiao <a92304a92304@gmail.com>
2022-04-18 07:25:00 +0800

    refactor: extract layout component

--- src/pages/Popup/App.tsx
+++ src/pages/Popup/App.tsx
@@ -4,45 +4,37 @@ import Footer from './components/Footer'
 import Switch from './components/Switch'
 import GoToQuestion from './components/GoToQuestion'
 import Tabs, { useTabs } from './components/Tabs'
-import styled from 'styled-components'
 import useEnableDarkTheme from './hooks/useEnableDarkTheme'
-
-const AppLayout = styled.div({ height: '100%' })
+import Layout from './components/Layout'
+import If from './components/If'
 
 export default function App () {
   const { tab, setTab, isTabDarkTheme, isTabQuestions } = useTabs()
   const { isDarkThemeEnabled, setIsDarkThemeEnabled } = useEnableDarkTheme()
 
   return (
-    <AppLayout className="ts-app-layout is-vertical">
-      <div className="cell">
+    <Layout
+      header={<>
         <div className="ts-content is-tertiary">
           <Logo />
         </div>
         <Tabs value={tab} onChange={setTab} />
-      </div>
+      </>}
+      body={<>
+        <If is={isTabQuestions}>
+          <h2 className="ts-header">Questions</h2>
+          <GoToQuestion />
+        </If>
 
-      <div className="cell is-scrollable" style={{ flex: 1 }}>
-        <main className="ts-content">
-          {isTabQuestions && <>
-            <h2 className="ts-header">Questions</h2>
-            <GoToQuestion />
-          </>}
-
-          {isTabDarkTheme && <>
-            <Switch
-              checked={isDarkThemeEnabled}
-              onChange={(e) => setIsDarkThemeEnabled(e.target.checked)}
-            >Enable Dark Theme</Switch>
-            <Options disabled={!isDarkThemeEnabled} />
-          </>}
-
-          <div className="ts-space" />
-        </main>
-      </div>
-      <div className="cell">
-        <Footer />
-      </div>
-    </AppLayout>
+        <If is={isTabDarkTheme}>
+          <Switch
+            checked={isDarkThemeEnabled}
+            onChange={(e) => setIsDarkThemeEnabled(e.target.checked)}
+          >Enable Dark Theme</Switch>
+          <Options disabled={!isDarkThemeEnabled} />
+        </If>
+      </>}
+      footer={<Footer />}
+    />
   )
 }
