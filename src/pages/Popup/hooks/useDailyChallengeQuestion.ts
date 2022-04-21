e3ff7a0cb1982359a28500eaff095fd93c2afb30
Huang Xingqiao <a92304a92304@gmail.com>
2022-04-21 09:31:30 +0800

    refactor: extract function `fetchDailyChallengeQuestion`

--- src/pages/Popup/hooks/useDailyChallengeQuestion.ts
+++ src/pages/Popup/hooks/useDailyChallengeQuestion.ts
@@ -1,26 +1,15 @@
-import { useCallback, useEffect, useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useStorageState } from 'react-use-storage-state'
+import { fetchDailyChallengeQuestion } from '../modules/apis'
 import DailyChallengeQuestion from '../types/DailyChallengeQuestion'
 
 export default function useDailyChallengeQuestion () {
   const [dailyChallengeQuestion, setDailyChallengeQuestion] =
     useStorageState<DailyChallengeQuestion | null>('dailyChallenge', null)
 
-  const fetchDailyChallengeQuestion = useCallback(async () => {
-    const res = await fetch('https://leetcode.com/graphql/', {
-      headers: { 'content-type': 'application/json' },
-      body: '{"query":"\\n    query questionOfToday {\\n  activeDailyCodingChallengeQuestion {\\n    date\\n    userStatus\\n    link\\n    question {\\n      acRate\\n      difficulty\\n      freqBar\\n      frontendQuestionId: questionFrontendId\\n      isFavor\\n      paidOnly: isPaidOnly\\n      status\\n      title\\n      titleSlug\\n      hasVideoSolution\\n      hasSolution\\n      topicTags {\\n        name\\n        id\\n        slug\\n      }\\n    }\\n  }\\n}\\n    ","variables":{}}',
-      method: 'POST',
-    })
-    const { data } = await res.json()
-    setDailyChallengeQuestion(
-      data.activeDailyCodingChallengeQuestion as DailyChallengeQuestion
-    )
-  }, [setDailyChallengeQuestion])
-
   useEffect(() => {
-    fetchDailyChallengeQuestion()
-  }, [fetchDailyChallengeQuestion])
+    fetchDailyChallengeQuestion().then(setDailyChallengeQuestion)
+  }, [setDailyChallengeQuestion])
 
   const dailyChallengeQuestionUrl = useMemo(() => {
     if (!dailyChallengeQuestion) return null
