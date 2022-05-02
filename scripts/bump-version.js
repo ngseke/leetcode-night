f50836ff1a3932ec84466d3be4ec6749de4cf7aa
Huang Xingqiao <a92304a92304@gmail.com>
2022-05-02 10:41:24 +0800

    chore: create bump version script

---
+++ scripts/bump-version.js
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs')
+const path = require('path')
+
+const fileName = 'package.json'
+const package = require(path.join('..', fileName))
+
+const type = process.argv[2]
+const { version } = package
+let [major, minor, patch] = version.split('.').map(Number)
+
+if (type === 'major') {
+  major += 1
+  minor = 0
+  patch = 0
+} else if (type === 'minor') {
+  minor += 1
+  patch = 0
+} else {
+  patch += 1
+}
+
+const newVersion = `${major}.${minor}.${patch}`
+
+package.version = newVersion
+
+const newPackage = JSON.stringify(package, 0, 2) + '\n'
+
+fs.writeFileSync(fileName, newPackage)
