591864b80a082100d75222dea2ca912b75754da1
Huang Xingqiao <a92304a92304@gmail.com>
2022-05-02 11:17:52 +0800

    chore: commit changes automatically

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -4,6 +4,12 @@ const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
 const { execSync } = require('child_process')
+const checkIfGitDirty = require('./checkIfGitDirty')
+
+if (!checkIfGitDirty()) {
+  console.log(chalk.red.bold('Error: There are uncommitted changes. Commit or stash them first.'))
+  process.exit(1)
+}
 
 const fileName = 'package.json'
 const package = require(path.join('..', fileName))
@@ -30,10 +36,15 @@ package.version = newVersion
 const newPackage = JSON.stringify(package, 0, 2) + '\n'
 
 fs.writeFileSync(fileName, newPackage)
+
 execSync('npm install', { stdio: 'inherit' })
+execSync('git add package.json package-lock.json', { stdio: 'inherit' })
+
+const commitMessage = `chore: bump version v${newVersion}`
+execSync(`git commit -m "${commitMessage}"`, { stdio: 'inherit' })
 
 console.log(
-  'Bumped version from ' +
+  '\nBumped version from ' +
   chalk.greenBright(version) +
   ' to ' +
   chalk.green.bold(newVersion)
