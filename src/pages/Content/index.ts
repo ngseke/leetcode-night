b0ccaf9047851cd1f3234bec0ef398133cc836dc
ngseke <ngseke@gmail.com>
2022-12-17 21:10:08 +0800

    feat: sync `isEnabled` with LeetCode's dark side

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -1,4 +1,4 @@
-import { loadIsEnabled, loadOptions, loadIsAutoResetCodeEnabled } from '../../storage'
+import { loadIsEnabled, loadOptions, loadIsAutoResetCodeEnabled, saveIsEnabled } from '../../storage'
 
 import { toggleEnabled } from './toggleEnabled'
 import { toggleInvertImageColor } from './toggleInvertImageColor'
@@ -13,6 +13,7 @@ import { ENABLED_STORAGE_KEY, OPTIONS_STORAGE_KEY } from '../../constants'
 import { resetCode } from './resetCode'
 import { resetCode2023 } from './resetCode2023'
 import { detectVersion } from './version'
+import { onChangeIsDarkSide2023, setIsDarkSide2023 } from './darkSide2023'
 
 async function toggleInvertImageColorByVersion (value: boolean) {
   const version = await detectVersion()
@@ -38,6 +39,15 @@ const handleOptionsChange = (options: OptionsForm) => {
   toggleHideLogoByVersion(options?.hideLogo ?? false)
 }
 
+async function toggleEnabledByVersion (value: boolean) {
+  const version = await detectVersion()
+  if (version === 2023) {
+    setIsDarkSide2023(value)
+  } else if (version === 2022) {
+    toggleEnabled(value)
+  }
+}
+
 async function resetCodeByVersion () {
   const version = await detectVersion()
   if (version === 2023) {
@@ -48,7 +58,17 @@ async function resetCodeByVersion () {
 }
 
 async function init () {
-  toggleEnabled(await loadIsEnabled())
+  chrome.storage.onChanged.addListener((changes) => {
+    const isEnabled = changes[ENABLED_STORAGE_KEY]?.newValue
+    if (isEnabled != null) toggleEnabledByVersion(isEnabled)
+
+    const options = changes[OPTIONS_STORAGE_KEY]?.newValue
+    if (options != null) handleOptionsChange(options)
+  })
+
+  toggleEnabledByVersion(await loadIsEnabled())
+  onChangeIsDarkSide2023(saveIsEnabled)
+
   handleOptionsChange(await loadOptions())
 
   if (await loadIsAutoResetCodeEnabled()) {
@@ -57,11 +77,3 @@ async function init () {
 }
 
 init()
-
-chrome.storage.onChanged.addListener((changes) => {
-  const isEnabled = changes[ENABLED_STORAGE_KEY]?.newValue
-  if (isEnabled != null) toggleEnabled(isEnabled)
-
-  const options = changes[OPTIONS_STORAGE_KEY]?.newValue
-  if (options != null) handleOptionsChange(options)
-})
