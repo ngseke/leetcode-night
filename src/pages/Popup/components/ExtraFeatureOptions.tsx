4b86bfe59d4bb4b87fe74b2dc2f122252e84fafb
ngseke <ngseke@gmail.com>
2024-01-29 21:03:15 +0800

    refactor: extract component OptionSwitch

--- src/pages/Popup/components/ExtraFeatureOptions.tsx
+++ src/pages/Popup/components/ExtraFeatureOptions.tsx
@@ -1,42 +1,31 @@
 import { useTranslation } from 'react-i18next'
-import { Switch } from './Switch'
 import { AUTO_RESET_CODE_ENABLED_STORAGE_KEY, INSERT_DISLIKE_COUNT_STORAGE_KEY, INSERT_YOUTUBE_LINK_STORAGE_KEY } from '../../../storage'
-import { useChromeStorage } from '../hooks/useChromeStorage'
+import { OptionSwitch } from './OptionSwitch'
 
 export function ExtraFeatureOptions () {
   const { t } = useTranslation()
 
-  const [isAutoResetCodeEnabled, setIsAutoResetCodeEnabled] =
-    useChromeStorage(AUTO_RESET_CODE_ENABLED_STORAGE_KEY)
-  const [isInsertYoutubeLinkEnabled, setIsInsertYoutubeLinkEnabled] =
-    useChromeStorage(INSERT_YOUTUBE_LINK_STORAGE_KEY)
-  const [isInsertDislikeCountEnabled, setIsInsertDislikeCountEnabled] =
-    useChromeStorage(INSERT_DISLIKE_COUNT_STORAGE_KEY)
-
   return (
     <div className="ts-wrap is-vertical is-start-aligned">
-      <Switch
-        checked={isAutoResetCodeEnabled ?? false}
-        onChange={setIsAutoResetCodeEnabled}
+      <OptionSwitch
+        storageKey={AUTO_RESET_CODE_ENABLED_STORAGE_KEY}
         icon="rotate-left"
       >
         {t('option.autoResetCode')}
-      </Switch>
-      <Switch
-        checked={isInsertYoutubeLinkEnabled ?? false}
-        onChange={setIsInsertYoutubeLinkEnabled}
+      </OptionSwitch>
+      <OptionSwitch
+        storageKey={INSERT_YOUTUBE_LINK_STORAGE_KEY}
         icon="youtube"
       >
         {t('option.insertYoutubeLink')}
-      </Switch>
-      <Switch
-        checked={isInsertDislikeCountEnabled ?? false}
-        onChange={setIsInsertDislikeCountEnabled}
+      </OptionSwitch>
+      <OptionSwitch
+        storageKey={INSERT_DISLIKE_COUNT_STORAGE_KEY}
         icon="thumbs-down"
       >
         {t('option.insertDislikeCount')}
         <span className="ts-badge is-small is-start-spaced">BETA</span>
-      </Switch>
+      </OptionSwitch>
     </div>
   )
 }
