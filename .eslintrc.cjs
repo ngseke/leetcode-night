a3bdb0e2a517ff787b7fc11e5736f2760deeb1cd
ngseke <ngseke@gmail.com>
2024-02-01 12:03:26 +0800

    chore: setup vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
     ecmaFeatures: { jsx: true },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
   },
   plugins: [
     'react',
---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    setupFiles: [
+      './__tests__/setup.ts',
+    ],
+    globals: true,
+    environment: 'jsdom',
+    testTimeout: 10000,
+  },
+})
