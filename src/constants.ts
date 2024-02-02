b2b6a1790e7a3093be19798ed78d38556fd2bb09
ngseke <ngseke@gmail.com>
2024-02-02 15:16:09 +0800

    feat: optimize render of YouTube link

--- src/constants.ts
+++ src/constants.ts
@@ -6,8 +6,6 @@ export const EXTENSION_ID = 'aaokgipfeeeciodnffigjfiafledhcii'
 
 export const POWERED_BY_TEXT = '🪄 Powered by LeetCode Night'
 
-export const INSERT_YOUTUBE_LINK_DEBOUNCE_DELAY = 300
-
 export const TEST_IDS = {
   questionsTab: 'questionsTab',
   optionsTab: 'optionsTab',
