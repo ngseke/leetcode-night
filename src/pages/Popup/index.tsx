e1643400ec08475e45e855bb8f52f1f23dc96159
ngseke <ngseke@gmail.com>
2024-02-06 08:55:26 +0800

    refactor: use `ReactDOM.createRoot` instead of `render`

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -1,9 +1,17 @@
-import { render } from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
 import 'tocas/dist/tocas.min.css'
 import './index.css'
 
 import './i18n'
+const root = document.getElementById('app-container')
 
-render(<App />, window.document.querySelector('#app-container'))
+if (!root) throw new Error('Failed to select the root!')
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
